737f817f576fe27b81e99c352cedd40d92584d52
wanghaidi <2792026415@qq.com>
2022-02-11 16:41:45 +0800

    Initial commit

---
+++ day01/src/com/whd/indentifier/IndentFieryTest.java
@@ -0,0 +1,24 @@
+package com.whd.indentifier;
+
+import static java.lang.System.*;
+
+/**
+ * @author wanghaidi
+ * @create 2022-02-11 16:23
+ * 标识符的使用
+ *  1.标识符：凡是可以自己命名的地方都是标识符
+ *      例如：类名,变量名,方法名,接口名,包名等等
+ *  2.标识符的命名规则
+ *      *由26个英文字母大小写，0-9 ，_或 $ 组成 数字不可以开头。
+ *      *不可以使用关键字和保留字，但能包含关键字和保留字。
+ *      *Java中严格区分大小写，长度无限制。
+ *      *标识符不能包含空格.
+ */
+public class IndentFieryTest {
+    /**
+     * @param args 程序入口的参数
+     */
+    public static void main(String[] args) {
+        out.println("您好");
+    }
+}
